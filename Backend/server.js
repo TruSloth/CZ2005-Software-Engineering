bdf55cd2bae4995134bdacaba50abbcf61b53651
cheekychy <tzeminhng@gmail.com>
2022-03-24 12:09:15 +0800

    W

--- Backend/server.js
+++ Backend/server.js
@@ -6,15 +6,14 @@ const cors = require("cors");
 const register = require("./routes/register");
 const login = require("./routes/login");
 const queue = require("./routes/queue");
-const {createServer} = require('http');
-const {Server} = require('socket.io');
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
 dotenv.config();
 
-
 mongoose.connect(process.env.DATABASE_ACCESS, () =>
-    console.log("Database connected"));
-
+  console.log("Database connected")
+);
 
 app.use(express.json());
 app.use(cors());
@@ -27,26 +26,26 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "*"
+    origin: "*",
   },
-})
+});
 
 io.on("connection", (socket) => {
-  socket.on('join-room', (room) => {
+  socket.on("join-room", (room) => {
     socket.join(room);
-  })
+  });
 
-  socket.on('send-chat-message', (msg, room) => {
-    socket.to(room).emit('received-message', msg)
-  })
+  socket.on("send-chat-message", (msg, room) => {
+    socket.to(room).emit("received-message", msg);
+  });
 
-  socket.on('leave-room', (room) => {
-    socket.leave(room)
-  })
+  socket.on("leave-room", (room) => {
+    socket.leave(room);
+  });
 
-  socket.on('leave-queue', () => {
-    socket.disconnect(false)
-  })
-})
+  socket.on("leave-queue", () => {
+    socket.disconnect(false);
+  });
+});
 
 httpServer.listen(4000, () => console.log("Server is up"));
