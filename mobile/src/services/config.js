3f442e6aa1faba149b8278ff9fab051294675381
Lucas Foo <39505341+TruSloth@users.noreply.github.com>
2022-04-17 18:36:56 +0800

    Add files via upload

---
+++ mobile/src/services/config.js
@@ -0,0 +1,4 @@
+//export const LOCALHOST = 'YOUR_IP_ADDRESS' // For Localhost
+export const LOCALHOST = '18.140.248.164:80' // For AWS
+export const FLASKHOST = '13.214.158.202:80'
+export const GOOGLE_WEBCLIENT_ID = '147684251205-25g5quinj2fouufbsu75q31kdi62mln7.apps.googleusercontent.com'
